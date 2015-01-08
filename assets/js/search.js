ad063c8ae33a6943580b8598b6886ddcdec7a8df
rorygreig <rorygreig@gmail.com>
2015-01-08 18:14:29 +0000

    create table to display search results

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,8 +2,35 @@ function search (query){
 
 	$.get('/targets/search', {names: query}, function(response){
 		console.log(response);
-		// update results div
+		var data = JSON.parse(response);
 
+		// display results
+		var result = formatResult(data);
+		console.log(result);
+		// $('#searchResult').html("<p>"+ result + "<p/>");
+		populateTable('searchResult', result)	;
 	})
 
+	function formatResult(input){
+		var values = input[0];
+		var result = Object();
+		result.name = values.first_name + " " + values.last_name;
+		if(values.list_type.indexOf("PEP") > -1){
+			result.type = "Politically Exposed Person";
+		} else {
+			result.type = "Sanction List";
+		}
+		return result;
+	}
+
+	function populateTable(tableID, values){
+		var table = $('#' + tableID);
+		$(table).empty();
+
+		for (var prop in values) {
+			console.log(values[prop]);
+			var row = "<tr><td>" + prop + "</td><td>" + values[prop] + "</td></tr>";
+			$(table).append(row);
+		}
+	}
 }
