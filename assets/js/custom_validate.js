815428655f1b8caf3b96a17af1fa8b260c573e87
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2015-01-08 22:06:34 +0000

    Further work on signup form validation

---
+++ assets/js/custom_validate.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	// see http://jqueryvalidation.org/ for more information
+	$('#sign-up-form').validate({
+		rules: {
+			first_name: {
+				required: true,
+				minlength 2
+			},
+			last_name: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				required: true,				
+				minlength: 6
+			},
+			confirmation: {
+				required: true,
+				minlength: 6,
+				equalTo: '#password'
+			}
+		},
+		message: {
+			first_name: {
+				require: 'Please enter your first name.'
+			},
+			confirmation: {
+				equalTo: 'Please enter the same password as above.'
+			}
+		}
+	});
+});
